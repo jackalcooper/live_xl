59ffe345e6f2fee08c80c9cdff359eff5b738b88
tsai <jackalcooper@gmail.com>
2024-03-04 16:39:45 +0800

    add test case for worker crash

--- test/py_run_test.exs
+++ test/py_run_test.exs
@@ -56,4 +56,35 @@ defmodule LiveXLWeb.PyRunTest do
            ) ==
              {:timeout, {NimblePool, :checkout, [pool]}}
   end
+
+  test "crash restart" do
+    available_gpu_ids = 0..3
+    pool = CrashRestart
+
+    child =
+      {NimblePool,
+       worker: {WorkerPool, {:python_runner, available_gpu_ids: available_gpu_ids}},
+       name: pool,
+       pool_size: Enum.count(available_gpu_ids)}
+
+    {:ok, _} = Supervisor.start_link([child], strategy: :one_for_one)
+
+    msg = %{
+      action: :echo,
+      ref: inspect(make_ref()),
+      payload: %{"prompt" => "dog", "seed" => 44}
+    }
+
+    for _ <- 0..10 do
+      crash_msg = %{msg | action: :crash}
+
+      assert catch_exit(LiveXL.WorkerClient.command(pool, crash_msg, receive_timeout: 300)) ==
+               :receive_timeout
+    end
+
+    for _ <- 0..10 do
+      assert %{"prompt" => "dog", "seed" => 44} ==
+               LiveXL.WorkerClient.command(pool, msg)["payload"]
+    end
+  end
 end
