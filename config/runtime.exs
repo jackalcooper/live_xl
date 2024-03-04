cf8eaf47217b3321c8c766263deb33eb1f08da1b
tsai <jackalcooper@gmail.com>
2024-03-04 10:50:34 +0800

    refine env defaults

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,15 +33,15 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  scheme = System.get_env("PHX_SCHEME") || "https"
-  phx_port = System.get_env("PHX_PORT") || "4000"
+  host = System.get_env("PHX_HOST")
+  phx_port = System.get_env("PHX_PORT")
+  scheme = System.get_env("PHX_SCHEME")
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :live_xl, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :live_xl, LiveXLWeb.Endpoint,
-    url: [host: host, port: 443, scheme: scheme],
+    url: [host: host || "example.com", port: phx_port || 443, scheme: scheme || "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
