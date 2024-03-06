9c8e8a46999821258f876261292d7f34e47a2b0f
jackalcooper <jackalcooper@gmail.com>
2024-03-06 10:29:06 +0800

    add negative_prompt to args map

--- lib/live_xl_web/live/prompt.ex
+++ lib/live_xl_web/live/prompt.ex
@@ -86,7 +86,8 @@ defmodule LiveXLWeb.PromptLive do
       |> Map.put("height", 1024)
       |> Map.put("width", 1024)
       |> Map.put("guidance_scale", 0)
-
+      |> Map.put("negative_prompt", "blurry, blur, text, watermark, render, 3D, NSFW, nude, CGI, monochrome, B&W, cartoon, painting, smooth, plastic, blurry, low-resolution, deep-fried, oversaturated")
+|> dbg
     res =
       :timer.tc(fn -> LiveXL.Infer.run(%{action: "infer_lightning", payload: %{args: args}}) end)
 
