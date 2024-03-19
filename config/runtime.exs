ebf1e7176c69bc9a4543bb4423568a454574fa3e
tsai <jackalcooper@gmail.com>
2024-03-19 15:33:53 +0800

    handle SPACE_REPO_NAME

--- config/runtime.exs
+++ config/runtime.exs
@@ -57,6 +57,21 @@ if config_env() == :prod do
     check_origin: check_origin,
     secret_key_base: secret_key_base
 
+  if repo_name = System.get_env("SPACE_REPO_NAME") do
+    "#{System.get_env("SPACE_AUTHOR_NAME")}-#{repo_name}.hf.space"
+    |> String.replace("_", "-")
+    |> String.downcase(:ascii)
+
+    Application.put_env(:phoenix_demo, PhoenixDemo.Endpoint,
+      url: [host: host],
+      http: [
+        ip: {0, 0, 0, 0, 0, 0, 0, 0},
+        port: String.to_integer(System.get_env("PORT") || "4000"),
+        transport_options: [socket_opts: [:inet6]]
+      ]
+    )
+  end
+
   # ## SSL Support
   #
   # To get SSL working, you will need to add the `https` key
